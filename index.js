57684f5a6ac3a5d25dac9b36f78c7b5233715b1b
samuel <samuelhaileabg@gmail.com>
2023-05-26 04:58:58 +1000

    numbers

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+//task 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+
+//task 2
+let random = Math.ceil(Math.random() * 100)
+
+console.log(random);
+
+
+//task 3
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod);
+
+
+//task 4
+let numSet = [5, 12, 20, 8, 16];
+let max = Math.max(...numSet);
+
+console.log(max);
+
